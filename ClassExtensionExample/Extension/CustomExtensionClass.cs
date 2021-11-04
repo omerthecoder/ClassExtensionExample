a2273661ca5f13c7322233c36336757c908a6d8b
omerthecoder <okanra@uaa.k12.tr>
2021-11-04 13:38:27 +0300

    Add project files.

---
+++ ClassExtensionExample/Extension/CustomExtensionClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExtensionExample.Extension
+{
+    public static class CustomExtensionClass
+    {
+        public static string HarfDegistir(this string str)
+        {
+            char[] cd = new char[str.Length];
+            string ss = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                cd[i]= str[i];
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(str[i]!='a') cd[i] = '*';
+                
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                ss += cd[i];
+            }
+           
+            return ss;
+        }
+    }
+}
